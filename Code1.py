1fa3b0d53dc70e0ee7181e99390c5339806308f3
Ankushsr20 <64484790+Ankushsr20@users.noreply.github.com>
2020-05-26 03:47:50 +0530

    final Code1.py

--- Code1.py
+++ Code1.py
@@ -115,6 +115,10 @@ test_images_norm = test_images / 255.0
 
 
 model = models.Sequential()
+
+
+
+
 model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1) ))
 model.add(layers.Conv2D(64, (3,3), activation='relu'))
 model.add(layers.Conv2D(128, (3,3), activation='relu'))
