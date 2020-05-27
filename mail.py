026678b9480ee1559ee986fdb8dfd92525c179d8
Ankushsr20 <64484790+Ankushsr20@users.noreply.github.com>
2020-05-28 02:26:18 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -5,7 +5,8 @@ try:
     #initializing the server connection
     yag = yagmail.SMTP(user='testingpyp@gmail.com', password='Test_1234')
     #sending the email
-    yag.send(to='sabarishsabarish244@gmail.com', subject='Machine Learning Model Details', contents='Model Built!')
+    yag.send(to='sabarishsabarish244@gmail.com', subject='Machine Learning Model Details', contents='Congratulations Model Built Successfully!')
     print("Email sent successfully")
 except:
     print("Error, email was not sent")
+    
