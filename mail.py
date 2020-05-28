c851bda8fbabfcc3072d1ce0fb43301480e78c47
Ankushsr20 <64484790+Ankushsr20@users.noreply.github.com>
2020-05-29 04:24:55 +0530

    mail.py updated

--- mail.py
+++ mail.py
@@ -3,7 +3,7 @@ import yagmail
 
 try:
     #initializing the server connection
-    yag = yagmail.SMTP(user='testingpymail@gmail.com', password='Test_1234')
+    yag = yagmail.SMTP(user='your email', password='your pwd')
     #sending the email
     yag.send(to='srank2000@gmail.com', subject='Machine Learning Model Details', contents='Congratulations! Model Built Successfully!')
     print("Email sent successfully")
